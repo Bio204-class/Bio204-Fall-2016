450c58329c2c78c07898afd306f030ae5f749091
Paul Magwene <paul.magwene@duke.edu>
2016-11-15 17:01:16 -0500

    Added slides on power.  Some rearranging of earlier material.

---
+++ slides/describing-univariate-distns/auto/slides-descriptive-stats.el
@@ -0,0 +1,42 @@
+(TeX-add-style-hook
+ "slides-descriptive-stats"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../mybeamerstyle"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline14"
+    "sec:orgheadline24"
+    "sec:orgheadline26")
+   (LaTeX-add-xcolor-definecolors
+    "bg"))
+ :latex)
+
